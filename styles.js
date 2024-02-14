88b413bc94119927b105d573666be4007f9fead8
fafahk <131696773+fafahk@users.noreply.github.com>
2024-02-14 11:17:14 -1100

    Add files via upload

---
+++ styles.js
@@ -0,0 +1,22 @@
+function goToPage2() {
+    window.location.href = 'page2.html'; // Redirect to page2.html
+}
+function goToPage3() {
+    window.location.href = 'page3.html'; // Redirect to page3.html
+}
+function goToPage4() {
+    window.location.href = 'page4.html'; // Redirect to page3.html
+}
+function goToPage5() {
+    window.location.href = 'page5.html'; // Redirect to page3.html
+}
+
+function goToPage6() {
+    window.location.href = 'page6.html'; // Redirect to page3.html
+}
+function goToPage7() {
+    window.location.href = 'page7.html'; // Redirect to page3.html
+}
+
+
+
